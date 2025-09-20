807118c2e16430138d14cb2129ca0395aa992a7a
BhagwatHudge123 <bhagwathudge96@gmail.com>
2025-09-20 10:15:47 +0000

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -1,46 +1,44 @@
 #!/usr/bin/env bash
 
 SRVPORT=4499
-RSPFILE=response
-
-rm -f $RSPFILE
-mkfifo $RSPFILE
 
 get_api() {
-	read line
-	echo $line
+    read line
+    echo $line
 }
 
 handleRequest() {
-    # 1) Process the request
-	get_api
-	mod=`fortune`
+    # Process the request
+    get_api
+    mod=`fortune`
 
-cat <<EOF > $RSPFILE
-HTTP/1.1 200
-
-
-<pre>`cowsay $mod`</pre>
-EOF
+    # Directly respond with cowsay
+    echo "HTTP/1.1 200 OK"
+    echo "Content-Type: text/html"
+    echo ""
+    echo "<pre>$(cowsay $mod)</pre>"
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
-		{ 
-			echo "Install prerequisites."
-			exit 1
-		}
+    command -v cowsay >/dev/null 2>&1 &&
+    command -v fortune >/dev/null 2>&1 &&
+    command -v nc >/dev/null 2>&1 || 
+        { 
+            echo "Install prerequisites."
+            exit 1
+        }
 }
 
 main() {
-	prerequisites
-	echo "Wisdom served on port=$SRVPORT..."
+    prerequisites
+    echo "Wisdom served on port=$SRVPORT..."
 
-	while [ 1 ]; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
-		sleep 0.01
-	done
+    # Listen for connections, respond with handleRequest
+    while true; do
+        # Handle incoming requests using nc
+        nc -lN $SRVPORT | handleRequest
+        sleep 0.01
+    done
 }
 
 main
